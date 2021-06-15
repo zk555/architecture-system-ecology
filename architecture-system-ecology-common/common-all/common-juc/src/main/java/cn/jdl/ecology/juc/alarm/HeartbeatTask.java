8c15c5365946982cee10efb0c0940ef4c739fcce
zhaokai108 <zhaokai108@jd.com>
2021-06-15 21:11:17 +0800

    Thread

---
+++ architecture-system-ecology-common/common-all/common-juc/src/main/java/cn/jdl/ecology/juc/alarm/HeartbeatTask.java
@@ -0,0 +1,15 @@
+package cn.jdl.ecology.juc.alarm;
+
+/**
+ * @Description:
+ * @Author zhaokai108
+ * @Date 2021/6/15 20:50
+ * @Version 1.0
+ * @Copyright: Copyright (c)2020 JDL.CN All Right Reserved
+ */
+public class HeartbeatTask implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
