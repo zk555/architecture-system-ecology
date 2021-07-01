6559b41309fe7348c36b0402bb7e460f2cfb5619
zhaokai108 <zhaokai108@jd.com>
2021-07-01 15:30:50 +0800

    补充id生成器

---
+++ architecture-system-ecology-common/common-all/common-id-generator/src/main/java/cn/jdl/ecology/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @Description: 雪花生成器
+ * @Author zhaokai108
+ * @Date 2021/7/1 11:17
+ * @Version 1.0
+ * @Copyright: Copyright (c)2020 JDL.CN All Right Reserved
+ */
+package cn.jdl.ecology;
